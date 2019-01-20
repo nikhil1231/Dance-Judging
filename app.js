1c4f3905eecd19e21f8dfe067d6a86ac0bcd7911
nikhil1231 <nikhil1231@hotmail.co.uk>
2019-01-20 14:41:51 +0000

    upload video files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+var formidable = require('formidable');
 var privateKey  = fs.readFileSync('server.key', 'utf8');
 var certificate = fs.readFileSync('server.crt', 'utf8');
 var MongoClient = require('mongodb').MongoClient;
@@ -17,9 +18,25 @@ var url = "mongodb://localhost:27017/mydb";
 //   db.close();
 // });
 
-app.get("/", function(req, res) {
-	console.log("successful test")
-	res.sendFile(__dirname + "/www/index.html")
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/www/index.html')
+})
+
+app.post('/fileupload', (req, res) => {
+	var form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+    	const d = new Date();
+    	// Create unique video file name.
+    	const filename = d.getTime() + "_" + Math.floor((Math.random() * 9000) + 1000);
+		const extension = files.filetoupload.name.split('.').pop();
+		const oldpath = files.filetoupload.path;
+		const newpath = __dirname + '/videos/' + filename + '.' + extension;
+		fs.rename(oldpath, newpath, (err) => {
+			if (err) throw err;
+			res.write('Video successfully uploaded.');
+			res.end();
+		});
+	})
 })
 
 var httpServer = http.createServer(app);
